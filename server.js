af1c21bcb4f0d388074091de8c131474d31afe98
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-09-13 16:22:03 -0700

    get, post, delete

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import app from "./lib/app.js";
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 const hostname = process.env.HOST || 'localhost';
 
 server.listen(PORT, hostname, () => {
