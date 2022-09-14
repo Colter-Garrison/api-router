af1dbd183d713c00dad70081e7e8d1f3c969e991
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-09-13 18:01:10 -0700

    two tests and routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import http from "node:http";
 import app from "./lib/app.js";
 
-const server = http.createServer(app);
+export const server = http.createServer(app);
 
 const PORT = process.env.PORT || 8080;
 const hostname = process.env.HOST || 'localhost';
